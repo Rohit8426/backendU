852bbbea1f324f4b46425188bdacdc79fed931b5
Rohit <rohit7067842611@gmail.com>
2024-12-01 16:10:13 +0530

    config changes

--- config/config.js
+++ config/config.js
@@ -16,29 +16,30 @@ development : {
   host: '127.0.0.1',
   dialect: 'postgres'
 },
- production : {
-  username: 'postgres',
-  password: '12345',
-  database: 'UMRAH',
-  host: '127.0.0.1',
-  dialect: 'postgres'
-}
+production: {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+  logging: false, // Disable logging in production
+  schema: 'umrahschema', // Specify your schema
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+},
+//  production : {
+//   username: 'postgres',
+//   password: '12345',
+//   database: 'UMRAH',
+//   host: '127.0.0.1',
+//   dialect: 'postgres'
+// }
 }
 
 
-// production: {
-//   username: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   host: process.env.DB_HOST,
-//   dialect: 'postgres',
-//   logging: false, // Disable logging in production
-//   schema: 'umrahschema', // Specify your schema
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false,
-//     },
-//   },
-// },
+
 // };
