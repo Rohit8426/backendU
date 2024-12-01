1aef9534e8ef8c11f76d24622750f4cbd9dac980
Rohit <rohit7067842611@gmail.com>
2024-12-01 19:58:49 +0530

    modified like first

--- config/database.js
+++ config/database.js
@@ -2,22 +2,22 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
 dotenv.config();
-// const sequelize = new Sequelize({
-//   dialect: 'postgres',
-//   host: process.env.DB_HOST || 'localhost',
-//   username: process.env.DB_USER || 'postgres',  
-//   password: process.env.DB_PASSWORD || '842611',  
-//   database: process.env.DB_NAME || 'umrah',
-//   database: process.env.DB_PORT,
-//   schema: 'umrahschema',
-//   logging: false,  
-// });
+const sequelize = new Sequelize({
+  dialect: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  username: process.env.DB_USER || 'postgres',  
+  password: process.env.DB_PASSWORD || '842611',  
+  database: process.env.DB_NAME || 'umrah',
+  database: process.env.DB_PORT,
+  schema: 'umrahschema',
+  logging: false,  
+});
 console.log('DATABASE_URL:', process.env.DATABASE_URL);
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  logging: false,
-});
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//   dialect: 'postgres',
+//   logging: false,
+// });
 
 
 
